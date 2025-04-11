d37ee7b23cebe4787d09277153d5aabbcb4a76ff
Shila <karamishila00@email.com>
2025-04-11 20:04:58 +0330

    Initial commit for final version

---
+++ sima/CarAgancyLogin/CarAgancyLogin/Views/LoginForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+using CarAgancyLogin.Controllers;
+using CarAgancyLogin.Helpers; // برای هش کردن رمز عبور
+
+namespace CarAgancyLogin.Views
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click_Click(object sender, EventArgs e)
+        {
+            // هش کردن رمز عبور وارد شده
+            string enteredPassword = txtPassword.Text;
+            string hashedPassword = PasswordHasher.HashPassword(enteredPassword);
+
+            // بررسی ورود کاربر با استفاده از کنترلر
+            UserController controller = new UserController();
+            bool success = controller.Login(txtUsername.Text, hashedPassword, txtcombobox.Text);
+
+            MessageBox.Show(success ? "ورود موفقیت‌آمیز بود!" : "نام کاربری یا رمز عبور اشتباه است.");
+        }
+
+        private void btnGoToRegister_Click(object sender, EventArgs e)
+        {
+            // انتقال به فرم ثبت‌نام
+            this.Hide();
+            RegisterForm registerForm = new RegisterForm();
+            registerForm.Show();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            // مقداردهی اولیه در صورت نیاز
+        }
+
+        private void btnForgotPassword_Click(object sender, EventArgs e)
+        {
+            // انتقال به فرم فراموشی رمز عبور
+            this.Hide();
+            ForgotPasswordForm forgot = new ForgotPasswordForm();
+            forgot.Show();
+        }
+    }
+}
